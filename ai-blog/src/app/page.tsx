85c09d0c786e14f5405bc5065ed6a2feff7eaefc
Abhijeet Surakanti <abhijeetsurakanti@gmail.com>
2023-06-17 16:25:29 +0530

    1. Created Next-App with tailwindcss, typescript
    2. Applied a CSS reset and configured tailwindcss
    
    3. Created a loading.tsx file and added some placeholder code

---
+++ ai-blog/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className=" min-h-screen flex flex-col justify-center items-center bg-gradient-gradual">
+      <div className="text-6xl">Hello</div>
+      <div>Hello</div>
+    </main>
+  );
+}
